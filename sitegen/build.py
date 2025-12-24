d4ccf6461776d0e58cf8240b7822ddfdf4466c19
itsdanjc <dan@itsdanjc.com>
2025-12-24 16:13:25 +0000

    Implemented read method for class Page

--- sitegen/build.py
+++ sitegen/build.py
@@ -1,10 +1,13 @@
+from pathlib import Path
 from datetime import datetime
 from marko import Markdown
-from marko.block import Document
+from marko.block import Document, Heading
 
+BLANK_PAGE_DEFAULT = "# {heading}\n{body}"
 ENABLED_MARKO_EXTENSIONS = frozenset(
     {'footnote', 'toc', 'codehilite', 'gfm'}
 )
+
 
 class Page(Markdown):
     """
@@ -12,9 +15,33 @@ class Page(Markdown):
     A subclass of `marko.Markdown`.
     """
 
-    title: str
+    title: Heading
     body: Document
+    document_path: Path
+    is_draft: bool
+    default_when_empty: str
     last_modified: datetime
 
-    def __init__(self):
+    def __init__(self, path: Path, default: str | None = ""):
         super().__init__(extensions=ENABLED_MARKO_EXTENSIONS)
+        self.document_path = path
+        self.default_when_empty = BLANK_PAGE_DEFAULT.format(
+            heading=self.document_path.stem.title(),
+            body=default,
+        )
+
+    def read(self) -> None:
+        """
+        Read and parse the markdown file at located at `self.document_path`.
+        :return:
+        """
+        with self.document_path.open("r", errors='replace') as f:
+            self.body = self.parse(f.read())
+
+        if len(self.body.children) == 0:
+            self.body = self.parse(self.default_when_empty)
+
+        first_child = self.body.children[0]
+        if isinstance(first_child, Heading):
+            self.title = first_child
+            self.body.children = self.body.children[1:]
