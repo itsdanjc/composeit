e462a9fef03f94bb1d1bca0773218862f2d9076e
itsdanjc <dan@itsdanjc.com>
2025-12-30 12:06:44 +0000

    Bump version 0.1.0

--- sitegen/__init__.py
+++ sitegen/__init__.py
@@ -3,7 +3,7 @@ from .context import BuildContext, FileType, BuildReason
 from .exec import BuildException, FileTypeError
 from .site import SiteRoot
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __author__ = "itsdanjc"
 __all__ = [
     "Page",
